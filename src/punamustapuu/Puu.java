ea9528d09fb1438d877fb317608428b0cb3eec5f
Roope <Roope@10.213.41.243>
2012-02-21 11:56:11 +0200

    lisätty Puu.java luokka

---
+++ src/punamustapuu/Puu.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punamustapuu;
+
+/**
+ *
+ * @author Roope
+ */
+public class Puu {
+    
+    private Solmu juuri;
+    
+    public Puu(Solmu juuri){
+        this.juuri = juuri;
+    }
+    
+}
