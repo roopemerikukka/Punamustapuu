c19bfa665c007950f5c0bb7aeba880cf00dd5a84
Roope <Roope@10.213.41.243>
2012-02-21 11:05:25 +0200

    lisätty Solmu luokka

---
+++ src/punamustapuu/Solmu.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punamustapuu;
+
+/**
+ *
+ * @author Roope
+ */
+public class Solmu {
+    
+    private static int avain;
+    private static String vari;
+    private static Solmu vanhempi;
+    private static Solmu vasen;
+    private static Solmu oikea;
+    
+    public Solmu(){
+        
+    }
+    
+}
