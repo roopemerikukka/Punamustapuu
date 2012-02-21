97c88083e7f1a4bb080b69d9e6d73e9b51205c8a
Joonas Salovaara <joonas.salovaara@gmail.com>
2012-02-21 11:25:10 +0200

    Lisätty metodeja punamustapuu-luokkaan

--- src/punamustapuu/Solmu.java
+++ src/punamustapuu/Solmu.java
@@ -11,13 +11,48 @@ package punamustapuu;
 public class Solmu {
     
     private static int avain;
-    private static String vari;
+    private static int vari;
     private static Solmu vanhempi;
     private static Solmu vasen;
     private static Solmu oikea;
     
-    public Solmu(){
-        
+    public Solmu(int avain){
+        this.avain = avain;
     }
     
+    /**
+     * 1 == musta, 0 == punainen
+     * @param vari 
+     */
+    private void asetaVari(int vari) {
+        this.vari = vari;
+    }
+    
+    private void asetaVasen(Solmu vasen) {
+        this.vasen = vasen;
+    }
+    
+    private void asetaOikea(Solmu oikea) {
+        this.oikea = oikea;
+    }
+    
+    private void asetaVanhempi(Solmu vanhempi) {
+        this.vanhempi = vanhempi;
+    }
+    
+    private int annaVari() {
+        return this.vari;
+    }
+    
+    private Solmu annaVasen() {
+        return this.vasen;
+    }
+    
+    private Solmu annaOikea() {
+        return this.oikea;
+    }
+    
+    private Solmu annaVanhempi() {
+        return this.vanhempi;
+    }
 }
