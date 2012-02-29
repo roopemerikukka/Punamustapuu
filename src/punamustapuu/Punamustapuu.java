1529dc2988620a3fa1261f1b8f3a3e5270d9b090
Joonas Salovaara <joonas.salovaara@gmail.com>
2012-02-29 12:48:14 +0200

    Kommentoitu Solmu-luokka, poistettu turhia rivejä muista luokista, asetettu tekijät.

--- src/punamustapuu/Punamustapuu.java
+++ src/punamustapuu/Punamustapuu.java
@@ -1,12 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package punamustapuu;
 
 /**
- *
- * @author Roope Merikukka & Joonas Salovaara
+ * 
+ * @author Joonas Salovaara && Roope Merikukka
  */
 public class Punamustapuu {
 
--- src/punamustapuu/Puu.java
+++ src/punamustapuu/Puu.java
@@ -1,12 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package punamustapuu;
 
 /**
- *
- * @author Roope
+ * 
+ * @author Joonas Salovaara && Roope Merikukka
  */
 public class Puu {
     
